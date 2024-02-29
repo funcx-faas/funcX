5eb9857611ed1cbb9d758f390415c4af56ba6115
Yadu Babuji <yadudoc1729@gmail.com>
2024-02-29 17:04:57 -0600

    Update GlobusComputeEngineBase to report exceptions at submit time along with the returned future

--- compute_endpoint/globus_compute_endpoint/engines/base.py
+++ compute_endpoint/globus_compute_endpoint/engines/base.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import logging
 import queue
 import threading
@@ -218,7 +219,11 @@ class GlobusComputeEngineBase(ABC):
                 "packed_task": packed_task,
                 "exception_history": [],
             }
-        future = self._submit(execute_task, task_id, packed_task, self.endpoint_id)
+        try:
+            future = self._submit(execute_task, task_id, packed_task, self.endpoint_id)
+        except Exception as e:
+            future = concurrent.futures.Future()
+            future.set_exception(e)
         self._setup_future_done_callback(task_id, future)
         return future
 
